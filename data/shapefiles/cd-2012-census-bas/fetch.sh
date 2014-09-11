d746c49f99c1c2812debd1471ba96764c50eef7a
Christian Torres <chtorrez@gmail.com>
2014-09-11 15:43:55 -0600

    Updating url where shapedfiles are fetched.

--- data/shapefiles/cd-2012-census-bas/fetch.sh
+++ data/shapefiles/cd-2012-census-bas/fetch.sh
@@ -10,6 +10,6 @@ mkdir -p data
 for fips in 01 02 04 05 06 08 09 10 11 12 13 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 44 45 46 47 48 49 50 51 53 54 55 56 60 66 69 72 78
 do
 	echo $fips;
-	wget -q -nc -O data/$fips.zip ftp://ftp2.census.gov/geo/pvs/$fips/partnership_shapefiles_12v2_$fips.zip;
+	wget -q -nc -O data/$fips.zip ftp://ftp2.census.gov/geo/pvs/$fips/partnership_shapefiles_14v1_$fips.zip;
 	unzip -q -u data/$fips.zip "*_cd_*";
 done
